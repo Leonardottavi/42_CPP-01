298df07f4dbe2083fccc5994472d05792e550120
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-11-20 14:48:55 +0100

    fix all

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,21 +6,32 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 15:19:57 by lottavi           #+#    #+#             */
-/*   Updated: 2024/11/14 15:48:12 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/11/20 14:31:53 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-// Constructor to initialize the name
-Zombie::Zombie(std::string zombieName) : name(zombieName) {}
+// Default constructor
+Zombie::Zombie() : name("Unnamed Zombie")
+{
+	std::cout << "A zombie has been created with the default constructor." << std::endl;
+}
 
-// Destructor to print a message when a zombie is destroyed
-Zombie::~Zombie() {
-	std::cout << name << " is destroyed." << std::endl;
+// Constructor to initialize the name
+Zombie::Zombie(std::string zombieName) : name(zombieName)
+{
+	std::cout << "A zombie named " << name << " has been created." << std::endl;
+}
+
+// Destructor
+Zombie::~Zombie()
+{
+	std::cout << "A zombie named " << name << " has been destroyed." << std::endl;
 }
 
 // Member function to announce the zombie
-void Zombie::announce() {
-	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+void Zombie::announce()
+{
+	std::cout << name << " is announcing: Braiiiiiiinnnssss..." << std::endl;
 }
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 15:16:46 by lottavi           #+#    #+#             */
-/*   Updated: 2024/11/14 15:48:06 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/11/20 14:31:06 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ private:
 	std::string name;
 
 public:
+	// Default constructor
+	Zombie();
+
 	// Constructor to initialize the name
 	Zombie(std::string zombieName);
 
